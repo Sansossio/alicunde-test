0ce4c00c0148c8b6a29df4b421654da546c0aa62
Julio Sansossio <juliosansossio@gmail.com>
2023-09-05 12:35:48 -0400

    nx template ajusts

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'alicunde-test',
+  displayName: 'api',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/alicunde-test',
+  coverageDirectory: '../../coverage/apps/api',
 };
